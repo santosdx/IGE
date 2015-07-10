c8bcc1ba7e113a1ad49779e960415bd81c59a293
SRojasM <SRojasM@DG_PC42.DANE.GOV.CO>
2015-07-10 07:43:35 -0500

    11-Commit [viernes_03-07-2015] Mejoras en las implementaciones del desarrollo de escritura del archivo excel.

--- GrupoEmpresarial-war/src/java/com/dane/ige/utilidad/Mensaje.java
+++ GrupoEmpresarial-war/src/java/com/dane/ige/utilidad/Mensaje.java
@@ -4,54 +4,58 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 /**
- * Clase que permite administrar los métodos de utilidad para el manejo de 
+ * Clase que permite administrar los métodos de utilidad para el manejo de
  * mensajes.
+ *
  * @author srojasm
  */
 public class Mensaje {
 
-
     /**
-     * Método que permite vizualizar un mensaje de tipo INFO; Recibe compo 
+     * Método que permite vizualizar un mensaje de tipo INFO; Recibe compo
      * parametro un String con el cuemrpo del mensaje y un String con el titulo
+     *
      * @param titulo
-     * @param mensaje 
+     * @param mensaje
      */
     public static void agregarMensajeGrowlInfo(String titulo, String mensaje) {
-        FacesContext context = FacesContext.getCurrentInstance();        
+        FacesContext context = FacesContext.getCurrentInstance();
         context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, titulo, mensaje));
-    } 
-    
+    }
+
     /**
-     * Método que permite vizualizar un mensaje de tipo WARN; Recibe compo 
+     * Método que permite vizualizar un mensaje de tipo WARN; Recibe compo
      * parametro un String con el cuemrpo del mensaje y un String con el titulo
+     *
      * @param titulo
-     * @param mensaje 
+     * @param mensaje
      */
     public static void agregarMensajeGrowlWarn(String titulo, String mensaje) {
         FacesContext context = FacesContext.getCurrentInstance();
         context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, titulo, mensaje));
     }
-     
+
     /**
-     * Método que permite vizualizar un mensaje de tipo ERROR; Recibe compo 
+     * Método que permite vizualizar un mensaje de tipo ERROR; Recibe compo
      * parametro un String con el cuemrpo del mensaje y un String con el titulo
+     *
      * @param titulo
-     * @param mensaje 
+     * @param mensaje
      */
     public static void agregarMensajeGrowlError(String titulo, String mensaje) {
         FacesContext context = FacesContext.getCurrentInstance();
         context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, titulo, mensaje));
     }
-    
-     /**
-     * Método que permite vizualizar un mensaje de tipo FATAL; Recibe compo 
+
+    /**
+     * Método que permite vizualizar un mensaje de tipo FATAL; Recibe compo
      * parametro un String con el cuemrpo del mensaje y un String con el titulo
+     *
      * @param titulo
-     * @param mensaje 
+     * @param mensaje
      */
     public static void agregarMensajeGrowlFatal(String titulo, String mensaje) {
         FacesContext context = FacesContext.getCurrentInstance();
         context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, titulo, mensaje));
-    }  
+    }
 }
